1a8139520de1d0098251f6bd0811d87aa12a6a82
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-07-26 21:55:33 +0900

    proj init

---
+++ iceStorage/iceStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  iceStorageApp.swift
+//  iceStorage
+//
+//  Created by 강민규 on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct iceStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
