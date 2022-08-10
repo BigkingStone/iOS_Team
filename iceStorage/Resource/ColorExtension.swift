9aa2e75a4df4424c84a7e2582d62d562c1708e88
seongBin <choidudghk@naver.com>
2022-08-10 16:23:25 +0900

    add color extensions

---
+++ iceStorage/Resource/ColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExtension.swift
+//  iceStorage
+//
+//  Created by 최성빈 on 2022/08/10.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let sdBlack = Color("sdBlack")
+    static let sdWhite = Color("sdWhite")
+    static let sdRed = Color("sdRed")
+    static let sdGreen = Color("sdGreen")
+    static let sdYellow = Color("sdBYellow")
+    static let sdOrange = Color("sdOrange")
+    static let sdBlue = Color("sdBlue")
+    static let sdGray = Color("sdGray")
+}
