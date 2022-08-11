3e18c69de714759e62d7dac34d8def67bcd4e424
강우승 <kung0615@gnu.ac.kr>
2022-08-12 00:59:00 +0900

    app linked firebase

--- iceStorage/App/iceStorageApp.swift
+++ iceStorage/App/iceStorageApp.swift
@@ -6,10 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 
 @available(iOS 14.0, *)
 @main
 struct iceStorageApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
