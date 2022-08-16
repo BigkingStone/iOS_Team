eb0b4428a6e3b13139fddd308d52fd12a9bac9be
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-08-16 22:47:45 +0900

    #2 gitignore del spm

---
+++ iceStorage/Presentation/Notification/NotificatinoView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificatinoView.swift
+//  iceStorage
+//
+//  Created by 강민규 on 2022/08/16.
+//
+
+import SwiftUI
+
+struct NotificatinoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificatinoView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificatinoView()
+    }
+}
